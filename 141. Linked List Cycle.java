71ac4314bf7454e11aa75f9e6f5a021fc4f415b0
SaeedGooda <Saeed.Gooda@ibm.com>
2024-03-26 16:42:10 +0200

    141. Linked List Cycle Problem Solution in Java

---
+++ 141. Linked List Cycle.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+
+    // First Solution Using HashSet
+    public boolean hasCycle(ListNode head) {
+        Set<ListNode> nodes = new HashSet<>();
+        while (head != null) {
+            if (nodes.contains(head)) {
+                return true;
+            } else {
+                nodes.add(head);
+            }
+            head = head.next;
+        }
+        return false;
+    }
+
+    // Second Solution using two pointers (optimal)
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast){
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
