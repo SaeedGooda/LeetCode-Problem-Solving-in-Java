a4d90f3c557d8067c6097e7246fc88cb86fffdfa
SaeedGooda <saeedgooda219@gmail.com>
2024-05-18 03:41:41 +0300

    Update 50. Pow(X, n) Solution

--- 50. Pow(x, n).java
+++ 50. Pow(x, n).java
@@ -1,22 +1,12 @@
 class Solution {
-    public double myPow(double x, int n) {
+    public double helper(double x, int n){
         if(n == 0) return 1;
-        if(n == 1) return x;
-        long N = n;
-        if (N < 0) {
-            x = 1 / x;
-            N = -N;
-        }
-        
-        double result = 1.0;
-        
-        while (N > 0) {
-            if ((N % 2) == 1) {
-                result *= x;
-            }
-            x *= x;
-            N /= 2;
-        }
-        return result;
+        double result = helper(x, n/2);
+        result = result * result;
+        return (n % 2 == 0)? result : x * result; 
+    }
+    public double myPow(double x, int n) {
+        double result = helper(x, n);
+        return n >= 0 ? result : 1 / result;
     }
 }
