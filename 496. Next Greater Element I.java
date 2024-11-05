1c6bf11cdedf3d63c7311fc3bc76028fe3e7608b
SaeedGooda <saeedgooda219@gmail.com>
2024-11-06 00:53:33 +0200

    Leetcode Problem Solving in Java

--- 496. Next Greater Element I.java
+++ 496. Next Greater Element I.java
@@ -24,3 +24,27 @@ class Solution {
 
 // Solution 2
 
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        HashMap<Integer, Integer> nextGreaterMap = new HashMap<>();
+        Stack<Integer> stack = new Stack<>();
+
+        for (int num : nums2) {
+            while (!stack.isEmpty() && stack.peek() < num) {
+                nextGreaterMap.put(stack.pop(), num);
+            }
+            stack.push(num);
+        }
+
+        while (!stack.isEmpty()) {
+            nextGreaterMap.put(stack.pop(), -1);
+        }
+
+        int[] result = new int[nums1.length];
+        for (int i = 0; i < nums1.length; i++) {
+            result[i] = nextGreaterMap.getOrDefault(nums1[i], -1);
+        }
+
+        return result;
+    }
+}
