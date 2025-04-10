30ccb15297ebb612bdba18f16e0ba83a9c3912fb
Saeed Gooda <saeed.goda-v@ejada.com>
2025-04-10 12:06:25 +0200

    Leetcode Problem Solving in Java

---
+++ 2130. Maximum Twin Sum of a Linked List.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public int pairSum(ListNode head) {
+        int result = 0;
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode tail = reverseList(slow);
+
+        while (tail != null) {
+            result = Math.max(result, head.val + tail.val);
+            head = head.next;
+            tail = tail.next;
+        }
+
+        return result;
+    }
+
+    private ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
+}
