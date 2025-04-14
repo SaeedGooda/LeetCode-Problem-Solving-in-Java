1d12f3a97b1b074b4bd7ed5bb2174b76fc38fc96
Saeed Gooda <saeed.goda-v@ejada.com>
2025-04-14 15:47:12 +0200

    Leetcode Problem Solving in Java

--- 74. Search a 2D Matrix.java
+++ 74. Search a 2D Matrix.java
@@ -12,35 +12,30 @@ class Solution {
     }
 }
 
-// Solution 2 --> Time Complexity = O(logn) + O(logm)
+// Solution 2 --> Time Complexity = O(log n * m)
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        int rows = matrix.length, cols = matrix[0].length;
-        int left = 0, right = cols - 1;
-        int top = 0, bottom = rows - 1;
-        int row = -1;
-        while(top <= bottom){
-            row = (top + bottom) / 2;
-            if(target > matrix[row][cols-1]) {
-                top = row + 1;
-            } else if(target < matrix[row][0]){
-                bottom = row - 1;
-            } else{
-                break;
-            }
-        }
-        if(row == -1){
+        if (matrix.length == 0){
             return false;
         }
-        while(left <= right){
+
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        
+        int left = 0;
+        int right = rows * cols;
+
+        while (left < right) {
             int mid = (left + right) / 2;
-            if(target == matrix[row][mid]){
+            int i = mid / cols;
+            int j = mid % cols;
+            
+            if (matrix[i][j] == target)
                 return true;
-            } else if(target > matrix[row][mid]){
+            if (matrix[i][j] < target)
                 left = mid + 1;
-            } else{
-                right = mid - 1;
-            }
+            else
+                right = mid;
         }
         return false;
     }
